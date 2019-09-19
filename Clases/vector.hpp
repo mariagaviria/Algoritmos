c25b08c37ff404f244582afa43c6c245c6c415c8
mariagaviria <44762498+mariagaviria@users.noreply.github.com>
2019-09-19 12:57:24 -0500

    Create vector.hpp

---
+++ Clases/vector.hpp
@@ -0,0 +1,24 @@
+#ifndef __vector_hpp
+#define __vector_hpp
+
+class Vector2D{
+private: // no se puede acceder directamente, ej: p.x -> NO se puede
+  double x;
+  double y;
+
+public: //si permite acceder directamente
+  Vector2D(); // sin ~ -> constructor
+  ~Vector2D(); // con ~ -> destructor
+
+  double norm();
+
+  void set_x(double v){
+    x=v;
+  }
+
+  void set_y(double n){
+    y=n;
+  }
+};
+
+#endif
