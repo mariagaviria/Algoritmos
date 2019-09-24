f48afe8a4e5de8453c7aa49514305c7cea18caf9
mariagaviria <44762498+mariagaviria@users.noreply.github.com>
2019-09-24 14:01:41 -0500

    Update vector.cpp

--- Clases/vector.cpp
+++ Clases/vector.cpp
@@ -10,7 +10,7 @@ Vector2D::~Vector2D(){}
 double Vector2D::norm(){
   return sqrt(x*x + y*y);
 }
-void Vector2D:: set_x(double v){
+void Vector2D::set_x(double v){
   x=v;
 }
 void Vector2D::set_y(double n){
@@ -21,4 +21,37 @@ double Vector2D::get_x(){
 }
 double Vector2D::get_y(){
   return y;
+}
+void Vector2D::operator=(Vector2D a){
+  x=a.get_x();
+  y=a.get_y();
+}
+/*Vector2D Vector2D::operator+(Vector2D a){
+  Vector2D n;
+  x+= a.get_x();
+  y+= a.get_y();
+  return n;
+}*/
+Vector2D operator+(Vector2D &a, Vector2D &b){
+  Vector2D m;
+  m.set_x(a.get_x() + b.get_x());
+  m.set_y(a.get_y() + b.get_y());
+  return m;
+}
+double operator*(Vector2D &a, Vector2D &b){
+  double m;
+  m=(a.get_x()*b.get_x())+(a.get_y()*b.get_y());
+  return m;
+}
+Vector2D operator*(Vector2D &a, double x){
+  Vector2D m;
+  m.set_x(a.get_x()*x);
+  m.set_y(a.get_y()*x);
+  return m;
+}
+Vector2D operator*(double x, Vector2D &a){
+  Vector2D m;
+  m.set_x(x*a.get_x());
+  m.set_y(x*a.get_y());
+  return m;
 }
