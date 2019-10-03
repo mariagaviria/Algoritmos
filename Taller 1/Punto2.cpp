c61bf8ee56d24e1b08eb929b5891198dfcfb5671
mariagaviria <44762498+mariagaviria@users.noreply.github.com>
2019-10-03 11:27:06 -0500

    Update Punto2.cpp

--- Taller 1/Punto2.cpp
+++ Taller 1/Punto2.cpp
@@ -1,1 +1,32 @@
-...
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+void print(vector<int> &s){
+	for (unsigned int i=0; i<s.size(); i++){
+		cout << ' ' << s[i]; 
+	}
+	cout << '\n'; 
+}
+
+vector<int> menor(vector<int> &w){
+	for (unsigned int i=w.size(); i>1; i--){
+		if (w[i] >=100){
+			w.pop_back();
+		}
+	}
+	return w;
+}
+
+int main(){
+	vector<int> vec;
+	for (unsigned int i=0; i<11; i++){
+		vec.push_back(pow(3, i));
+	}
+	print(vec);
+	vector<int> x=menor(vec);
+	print(x);
+	return 0;
+}
