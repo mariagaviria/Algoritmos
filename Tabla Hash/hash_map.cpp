7b8d5ba8c539421fe6e87311dd33394fe83ccf0e
mariagaviria <44762498+mariagaviria@users.noreply.github.com>
2019-11-14 19:47:15 -0500

    Update hash_map.cpp

--- Tabla Hash/hash_map.cpp
+++ Tabla Hash/hash_map.cpp
@@ -2,8 +2,11 @@ template <typename VT>
 HashMap<VT>::HashMap(VT def){
   def = notfound;
   tableSize = TABLE_SIZE;
-  table = new KeyValueNode[tableSize];
+  table = new KeyValueNode*[tableSize];
   count = 0;
+  for ( int i = 0; i < tableSize; i++){
+    table[i]=nullptr;
+  }
 }
 
 template <typename VT>
@@ -15,7 +18,14 @@ HashMap>VT>::~HashMap(){
 
 template <typename VT>
 KeyValueNode<VT>* HashMap<VT>::search_bucket(int index, string key){
-
+  KeyValueNode *recent = table[index];
+  while (recent != nullptr){
+    if (recent->key == key){
+      return recent;
+    }
+    recent = recent->next;
+  }
+  return recent;
 }
 
 template <typename VT>
@@ -35,17 +45,48 @@ void HashMap<VT>::clear(){
 
 template <typename VT>
 VT HashMap<VT>::get(string key){
-
+  int index = hash(key);
+  KeyValueNode *search = search_bucket(index, key)
+  if (search == nullptr){
+    return notfound;
+  }else{
+    return search->value;
+  }
 }
 
 template <typename VT>
 bool HashMap<VT>::search(string key){
-
+  KeyValueNode* find = search_bucket(hash(key), key);
+  return find != nullptr;
 }
 
 template <typename VT>
 bool HashMap<VT>::insert(string key, VT value){
-
+  bool find = search(key);
+  if (find == false){
+    int index = hash(key)
+    KeyValueNode* indexpointer = table[index];
+    if(indexpointer == nullptr){
+      KeyValueNode* newbox = new KeyValueNode;
+      indexpointer = newbox;
+      newbox->key = key;
+      newbox->value = value;
+      newbox->next = nullptr;
+    }else{
+      KeyValueNode* oldbox = indexpointer;
+      KeyValueNode* newbox = new KeyValueNode;
+      indexpointer = newbox;
+      newbox->key = key;
+      newbox->value = value;
+      newbox->next = oldbox;
+    }
+    count++;
+    return true;
+  }else{
+    KeyValueNode* search = search_bucket(hash(key), key);
+    search->value = value;
+    return false;
+  }
 }
 
 template <typename VT>
